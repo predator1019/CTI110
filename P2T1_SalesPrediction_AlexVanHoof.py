e68e1f56541efd033cd8281c865d9ac7b52a6a6d
predator1019 <43049539+predator1019@users.noreply.github.com>
2018-09-06 17:30:03 -0400

    first submission

---
+++ P2T1_SalesPrediction_AlexVanHoof.py
@@ -0,0 +1,13 @@
+# sales project
+# 9/5/18
+# CTI-110 P2T1 - Sales Prediction
+# Alex VanHoof
+
+# Get the projeted total sales
+total_sales = float(input('Enter the projected sales:'))
+
+# Calculate the profit as 23 percent of total sales
+profit = total_salses * 0.23
+
+#display the profit.
+print('the profit is $', format(profit, ',.2f'))
